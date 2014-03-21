f7dba0bcdb90edd8c5ba990c53897764a4a64d29
Tyler Treadwell <ttreads@live.com>
2014-03-21 01:08:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,22 @@
 session_start();
 session_destroy();
 ?>
-
+/**
+ * Copyright 2014 Tyler Treadwell and TTreads Designs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+ 
 <!DOCTYPE HTML>
 <head>
   <title>Dowling Social Network </title>
